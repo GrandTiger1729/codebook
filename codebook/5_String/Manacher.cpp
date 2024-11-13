663bb12c7641ae898557816375d792b2b56d8b6a
becaido <beck12832@gmail.com>
2024-11-13 17:41:08 +0800

    update Manacher

--- codebook/5_String/Manacher.cpp
+++ codebook/5_String/Manacher.cpp
@@ -1,14 +1,15 @@
-int z[MAXN]; // 0-base
+int z[SIZE]; // 0-base
 /* center i: radius z[i * 2 + 1] / 2
    center i, i + 1: radius z[i * 2 + 2] / 2
    both aba, abba have radius 2 */
 void Manacher(string tmp) {
   string s = "%";
-  int l = 0, r = 0;
-  for (char c : tmp) s.pb(c), s.pb('%');
-  for (int i = 0; i < SZ(s); ++i) {
+  int l = 0, r = 0, n;
+  for (char c : tmp) s.push_back(c), s.push_back('%');
+  n = s.size();
+  FOR (i, 0, n - 1) {
     z[i] = r > i ? min(z[2 * l - i], r - i) : 1;
-    while (i - z[i] >= 0 && i + z[i] < SZ(s) 
+    while (i - z[i] >= 0 && i + z[i] < s.size()
            && s[i + z[i]] == s[i - z[i]]) ++z[i];
     if (z[i] + i > r) r = z[i] + i, l = i;
   }

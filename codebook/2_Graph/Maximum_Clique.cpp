f2003d01bd8688cacaee570ec4afc428d2849efa
becaido <beck12832@gmail.com>
2024-11-13 17:59:06 +0800

    Update Maximum_Clique.cpp

--- codebook/2_Graph/Maximum_Clique.cpp
+++ codebook/2_Graph/Maximum_Clique.cpp
@@ -1,21 +1,21 @@
 struct Maximum_Clique {
-  typedef bitset<MAXN> bst;
-  bst N[MAXN], empty;
-  int p[MAXN], n, ans;
+  typedef bitset<SIZE> bst;
+  bst N[SIZE], empty;
+  int p[SIZE], n, ans;
   void BronKerbosch2(bst R, bst P, bst X) {
     if (P == empty && X == empty)
       return ans = max(ans, (int)R.count()), void();
     bst tmp = P | X;
     int u;
     if ((R | P | X).count() <= ans) return;
-    for (int uu = 0; uu < n; ++uu) {
+    FOR (uu, 0, n - 1) {
       u = p[uu];
       if (tmp[u] == 1) break;
     }
     // if (double(clock())/CLOCKS_PER_SEC > .999)
     // return;
     bst now2 = P & ~N[u];
-    for (int vv = 0; vv < n; ++vv) {
+    FOR (vv, 0, n - 1) {
       int v = p[vv];
       if (now2[v] == 1) {
         R[v] = 1;
@@ -26,7 +26,7 @@ struct Maximum_Clique {
   }
   void init(int _n) {
     n = _n;
-    for (int i = 0; i < n; ++i) N[i].reset();
+    FOR (i, 0, n - 1) N[i].reset();
   }
   void add_edge(int u, int v) {
     N[u][v] = N[v][u] = 1;
@@ -34,8 +34,8 @@ struct Maximum_Clique {
   int solve() { // remember srand
     bst R, P, X;
     ans = 0, P.flip();
-    for (int i = 0; i < n; ++i) p[i] = i;
-    random_shuffle(p, p + n), BronKerbosch2(R, P, X);
+    FOR (i, 0, n - 1) p[i] = i;
+    shuffle(p, p + n, rng), BronKerbosch2(R, P, X);
     return ans;
   }
-};
+} G;

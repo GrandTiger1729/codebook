f901f7bcd9c3248053137e469d46cdd9c954aa82
Beck Chen <beck12832@gmail.com>
2024-11-13 12:03:04 +0800

    update Z-value

--- codebook/5_String/Z-value.cpp
+++ codebook/5_String/Z-value.cpp
@@ -1,9 +1,9 @@
-int z[MAXn];
+int z[SIZE];
 void make_z(const string &s) {
-  int l = 0, r = 0;
-  for (int i = 1; i < SZ(s); ++i) {
+  int l = 0, r = 0, n = s.size();
+  FOR (i, 1, n - 1) {
     for (z[i] = max(0, min(r - i + 1, z[i - l]));
-         i + z[i] < SZ(s) && s[i + z[i]] == s[z[i]];
+         i + z[i] < n && s[i + z[i]] == s[z[i]];
          ++z[i])
       ;
     if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;

962eea8896e06c59341ec0a64c2fda6e43e3f510
becaido <beck12832@gmail.com>
2024-11-13 18:14:13 +0800

    Update Fast_Walsh_Transform.cpp

--- codebook/7_Polynomial/Fast_Walsh_Transform.cpp
+++ codebook/7_Polynomial/Fast_Walsh_Transform.cpp
@@ -5,7 +5,7 @@ invop: or, and, xor = -1, -1, 1/2 */
 void fwt(int *a, int n, int op) { //or
   for (int L = 2; L <= n; L <<= 1)
     for (int i = 0; i < n; i += L)
-      for (int j = i; j < i + (L >> 1); ++j)
+      FOR (j, i, j + (L >> 1) - 1)
         a[j + (L >> 1)] += a[j] * op;
 }
 const int N = 21;
@@ -13,18 +13,16 @@ int f[N][1 << N], g[N][1 << N], h[N][1 << N], ct[1 << N];
 void subset_convolution(int *a, int *b, int *c, int L) {
   // c_k = \sum_{i | j = k, i & j = 0} a_i * b_j
   int n = 1 << L;
-  for (int i = 1; i < n; ++i)
+  FOR (i, 1, n - 1)
     ct[i] = ct[i & (i - 1)] + 1;
-  for (int i = 0; i < n; ++i)
+  FOR (i, 0, n - 1)
     f[ct[i]][i] = a[i], g[ct[i]][i] = b[i];
-  for (int i = 0; i <= L; ++i)
+  FOR (i, 0, L)
     fwt(f[i], n, 1), fwt(g[i], n, 1);
-  for (int i = 0; i <= L; ++i)
-    for (int j = 0; j <= i; ++j)
-      for (int x = 0; x < n; ++x)
-        h[i][x] += f[j][x] * g[i - j][x];
-  for (int i = 0; i <= L; ++i)
+  FOR (i, 0, L) FOR (j, 0, i) FOR (x, 0, n - 1)
+    h[i][x] += f[j][x] * g[i - j][x];
+  FOR (i, 0, L)
     fwt(h[i], n, -1);
-  for (int i = 0; i < n; ++i)
+  FOR (i, 0, n - 1)
     c[i] = h[ct[i]][i];
 }

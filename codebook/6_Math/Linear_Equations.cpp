1d296122753136597a3658528536dce476d1636a
GrandTiger1729 <tiger7569.1@gmail.com>
2024-11-16 00:15:09 +0800

    Add find solution basis to system of linear equations

--- codebook/6_Math/Linear_Equations.cpp
+++ codebook/6_Math/Linear_Equations.cpp
@@ -1,7 +1,10 @@
-struct matrix { //m variables, n equations
+struct matrix { // m variables, n equations
   int n, m;
-  fraction M[MAXN][MAXN + 1], sol[MAXN];
-  int solve() { //-1: inconsistent, >= 0: rank
+  fraction M[MAXN][MAXN + 1], sol[MAXN], basis[MAXN][MAXN];
+	bool with_basis = true;
+  int rank = -1;
+  bool fixed[MAXN];
+  int solve() { // -1: inconsistent, >= 0: rank of solution space
     for (int i = 0; i < n; ++i) {
       int piv = 0;
       while (piv < m && !M[i][piv].n) ++piv;
@@ -9,15 +12,40 @@ struct matrix { //m variables, n equations
       for (int j = 0; j < n; ++j) {
         if (i == j) continue;
         fraction tmp = -M[j][piv] / M[i][piv];
-        for (int k = 0; k <= m; ++k) M[j][k] = tmp * M[i][k] + M[j][k];
+        for (int k = 0; k <= m; ++k)
+          M[j][k] = tmp * M[i][k] + M[j][k];
       }
     }
-    int rank = 0;
+    rank = m;
     for (int i = 0; i < n; ++i) {
       int piv = 0;
       while (piv < m && !M[i][piv].n) ++piv;
-      if (piv == m && M[i][m].n) return -1;
-      else if (piv < m) ++rank, sol[piv] = M[i][m] / M[i][piv];
+      if (piv == m && M[i][m].n) return rank = -1;
+      else if (piv < m) {
+        --rank;
+        if (with_basis) {
+          for (int j = m; j >= piv; j--)
+            M[i][j] = M[i][j] / M[i][piv];
+          sol[piv] = M[i][m];
+          fixed[piv] = true;
+        } else {
+          sol[piv] = M[i][m] / M[i][piv];
+        }
+      }
+    }
+    if (with_basis) {
+      for (int i = 0; i < n; ++i) { 
+				int piv = 0;
+				while (piv < m && !M[i][piv].n) ++piv;
+				for (int j = 0, k = 0; j < m; ++j) {
+					if (!fixed[j])
+						basis[k++][piv] = -M[i][j];
+				}
+			}
+			for (int j = 0, k = 0; j < m; ++j) {
+				if (!fixed[j])
+					basis[k++][j] = 1;
+			}
     }
     return rank;
   }
